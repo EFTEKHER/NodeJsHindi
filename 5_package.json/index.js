a11128ed7b10906d91ea5e7b7cd69fe1c5f440ce
EFTEKHER <eftekherali2000@gmail.com>
2023-06-21 22:22:44 +0600

    done

---
+++ 5_package.json/index.js
@@ -0,0 +1,17 @@
+//keeps project details
+
+console.log('package .json');
+
+//node package manager
+
+//npm init
+
+var colors=require('colors');
+console.log("hello".green);
+console.log("efte".blue);
+
+//node js single threaded , at a time one Command run
+
+const chalk = require('chalk');
+
+console.log(chalk.white.bgBlue.bold("The Big Bang Theory!"));
