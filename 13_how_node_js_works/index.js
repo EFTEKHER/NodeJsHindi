36f8190387ef979cdfc13d49edebfd0ce8afca2a
EFTEKHER <eftekherali2000@gmail.com>
2023-06-23 00:13:50 +0600

    done

---
+++ 13_how_node_js_works/index.js
@@ -0,0 +1,14 @@
+console.log('starting up');
+
+
+setTimeout(()=>{
+    console.log("2 second log");
+},2000)//c++ inherit  c++ stuff  store in node api
+
+
+setTimeout(()=>{
+console.log("0 second log");
+},0)//c++ c++ stuff  store in node api
+
+
+console.log('finished up');
